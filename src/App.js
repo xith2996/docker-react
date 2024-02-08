abc899dfd01bc8af170721d0dbacba76c99d256e
tanasak.l <tanasal.l@avlgb.com>
2024-02-08 17:52:47 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Learn react Hello There! Edit at Local
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Bye! 555 ABCDFE
         </a>
       </header>
     </div>
